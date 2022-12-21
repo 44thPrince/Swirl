a0a86a682c31b46216c183c1b568ebe095a335a9
44thPrince <84866119+44thPrince@users.noreply.github.com>
2022-12-21 08:42:05 -0500

    Completed Skeleton Program
    Added default directory, fixed indexing and fixed file display..

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,56 @@
 /*
-iced Architecture:
-    State — the state of your application
-    Messages — user interactions or meaningful events that you care about
-    View logic — a way to display your state as widgets that may produce messages on user interaction
-    Update logic — a way to react to messages and update your state
-    (My own addition) Persistence - how the application stores data that persists after the application is closed
-
 Planning - Split into subsections for each component of the architecture. Single indented are necessary features for the application to function, double or more are for unneccessary features or improvements
 
 Make skeleton program
     TODO: Align widgets to proper locations
-    TODO: Create pause play widget
+    Create pause play widget
     TODO: Fast forward//backward widgets
     TODO: Implement audio
 
 State:
-    TODO: Current directory (None if nothing is currently selected)
-    TODO: Store index of mp3 files, lazy load file currently playing and the two in front of and behind it using said index
-    TODO: Current file playing (None if nothing is selected)
-    TODO: Current state (enum) that stores whether song is paused or playing, or loading a new song
+    Current directory (None if nothing is currently selected)
+    Store index of mp3 files, lazy load file currently playing and the two in front of and behind it using said index
+    Current file playing (None if nothing is selected)
+    Current state (enum) that stores whether song is paused or playing, or loading a new song
 
 Messages:
-    TODO: Pause/play event
+    Pause/play event
         TODO: Next/prev track
-    TODO: Folder picker
+    Folder picker
         TODO: Emacs dired style folder picker (replaces above)
 
 View Logic:
-    TODO: Pause, play button
+    Pause, play button
         TODO: Next, prev track button
-    TODO: Open folder button -> folder picker ->
-    TODO: Show list of tracks in folder -> track selector
-        TODO: Shuffle play
+    File input box
+        Show list of tracks in folder -> track selector
+    TODO: Shuffle play
 
 Update Logic:
-    TODO: Change current directory when folder is opened
-    TODO: Selecting a track, or selecting next/prev track updates current state enum, current file playing
+    FIXME: Change current directory when folder is opened
+    Selecting a track, or selecting next/prev track updates current state enum, current file playing
 
     Persistence:
         TODO: Save current directory on program exit, load on program start. (optional)
             TODO: Save current track on program exit, load on program start. (optional) (depends on above)
 */
 
-use iced::alignment::{self, Alignment};
 use iced::event::{self, Event};
 use iced::keyboard;
 use iced::subscription;
-use iced::theme::{self, Theme};
+use iced::theme::Theme;
 use iced::widget::{
-    self, button, Button, checkbox, column, container, row, scrollable, text, text_input, Text, svg
+    self, button, column, container, row, scrollable, text, text_input, svg
 };
-use iced::window;
-use iced::{Application, Element};
-use iced::{Color, Command, Font, Length, Settings, Subscription, Renderer};
 
-use iced_native::Widget;
+use iced::{Application, Element};
+use iced::{Command, Settings, Subscription, Renderer};
+
 use once_cell::sync::Lazy;
 
-use std::fs::{self, File};
-
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::ops::ShrAssign;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use glob::glob;
-
 
 // use rodio::{Decoder, OutputStream, Sink};
 // use rodio::source::{SineWave, Source};
@@ -79,36 +64,59 @@ fn main() -> iced::Result {
     )
 }
 
-// custom button implementation
-
-
-// State
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct State {
     input_value: String,
-    cur_directory: String,
-    audio_index: Vec<PathBuf>,
-    track_names: Vec<String>,
+    cur_directory: PathBuf, //Change to PathBuf
+    tracks: Option<Vec<Track>>,
     // TODO: cur_track: Sink,
-    cur_track_name: String,
+    cur_track_name: Option<String>,
     playing: bool,
-    width: u32,
-    height: u32,
 }
 
-// Message
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            input_value: match dirs::home_dir() {
+                Some(home) =>
+                    format!("{}/", &home.as_path().display().to_string()),
+
+                None => String::from(""),
+            },
+            tracks: match dirs::home_dir() {
+                Some(home) => Some(index_cur_directory(&home)
+                            .iter()
+                            .map(|path|{
+                                Track::new(path.clone())
+                            })
+                            .collect()),
+
+                None => None,
+            },
+            cur_track_name: None,
+            cur_directory: match dirs::home_dir() {
+                Some(path) => [path.as_path(), &PathBuf::from("/")].iter().collect(),
+                None => PathBuf::new()
+
+            },
+            playing: false,
+        }
+    }
+}
+
+
 #[derive(Debug)]
 enum Player {
-    Loading,
+    Loading, // Will be unused until persistent state is added
     Loaded(State),
 }
 
-// Message
 #[derive(Debug, Clone)]
 enum Message {
-    LoadDirectory(String),
-    LoadTrack(PathBuf),
-    InputChanged(String),
+    LoadDirectory(PathBuf), //Load directory from directory path
+    InputChanged(String), // Change the
+    StartSong(PathBuf), //Play
     Play,
     Pause,
     // TODO: nextTrack,
@@ -116,7 +124,6 @@ enum Message {
     DownPressed,
     UpPressed,
     EnterPressed,
-    Resized(u32, u32)
 }
 
 // Multiple
@@ -127,6 +134,7 @@ impl Application for Player {
     type Executor = iced::executor::Default;
 
     fn new(_flags: ()) -> (Player, Command<Self::Message>) {
+        Message::LoadDirectory(State::default().cur_directory);
         (
             Player::Loaded(State::default()),
             // TODO: Command::perform( /* add load from save state function */ Message::Loaded)
@@ -137,7 +145,7 @@ impl Application for Player {
     fn title(&self) -> String {
         match self {
             Player::Loading => String::from("Swirl"),
-            Player::Loaded(state) => format!("Swirl is Playing: {}", if state.cur_track_name == "".to_string() { "Nothing" } else { &state.cur_track_name })
+            Player::Loaded(state) => format!("Swirl is Playing: {}", if state.cur_track_name == None { "Nothing" } else { state.cur_track_name.as_ref().unwrap() })
         }
     }
 
@@ -149,15 +157,25 @@ impl Application for Player {
             }
             Player::Loaded(state) => {
                 match message {
-                    Message::LoadDirectory(string) => {
-                        state.audio_index = index_cur_directory(&string);
-                        state.cur_directory = string;
+                    Message::LoadDirectory(path_buf) => {
+                        state.tracks = Some(index_cur_directory(&path_buf)
+                            .iter()
+                            .map(|path|{
+                                Track::new(path.clone())
+                            })
+                            .collect());
+                        state.cur_directory = path_buf;
                         return Command::none()
                     }
                     Message::LoadTrack(path) => {
-                        let path = get_file_names(vec![path]);
-                        state.cur_track_name = path[0].to_string();
+                        let path = get_file_name(&path);
+                        state.cur_track_name = Some(path.to_string());
                         return Command::none()
+                    }
+                    Message::StartSong(path) => {
+                        // TODO: Start audio playback
+                        Message::Play;
+                        return Command::none();
                     }
                     Message::Pause =>{
                         state.playing = false;
@@ -167,26 +185,17 @@ impl Application for Player {
                         state.playing = true;
                         return Command::none();
                     }
-                        //TODO: Implement
                     Message::DownPressed =>
                         widget::focus_next(),
-                        // Add check for custom widget containing tracks
                     Message::UpPressed =>
                         widget::focus_previous(),
-                        // Add check for custom widget containing tracks
                     Message::EnterPressed =>
-                        // Activate the widget focused on or play selected track
-                        // To be implemented later
+                        // TODO: Implement
                         Command::none(),
                     Message::InputChanged(value) => {
                         state.input_value = (&value).to_string();
-                        Message::LoadDirectory(value);
+                        Message::LoadDirectory(PathBuf::from(value));
                         return Command::none()
-                    }
-                    Message::Resized(width, height) => {
-                        state.width = width;
-                        state.height = height;
-                        return Command::none();
                     }
                 }
             }
@@ -199,8 +208,7 @@ impl Application for Player {
             Player::Loaded(State {
                 input_value,
                 playing,
-                height,
-                audio_index,
+                tracks,
                 ..
             }) => {
                 let toggle_play;
@@ -210,7 +218,6 @@ impl Application for Player {
                     Message::InputChanged)
                         .id(INPUT_ID.clone())
                         .padding(5);
-                audio_index = &index_cur_directory(&input_value);
                 match playing {
                     true => {
                         let pause = svg(svg::Handle::from_memory("
@@ -230,18 +237,16 @@ impl Application for Player {
      id='layer1'>
     <rect
        style='fill:#000000'
-       id='rect167'
-       width='2.2962112'
-       height='9.0195179'
-       x='4.5'
-       y='3.5' />
+       width='4.5'
+       height='14'
+       x='1.5'
+       y='1' />
     <rect
        style='fill:#000000'
-       id='rect167-3'
-       width='2.2962112'
-       height='9.0195179'
-       x='9.25'
-       y='3.5' />
+       width='4.5'
+       height='15'
+       x='10'
+       y='1' />
   </g>
 </svg>
 ".as_bytes()));
@@ -253,23 +258,15 @@ impl Application for Player {
 <svg
    fill='#000000'
    class='bi bi-play-fill'
-   viewBox='0 0 256 256'
+   viewBox='0 0 16 16'
    version='1.1'
    id='svg4'
    width='16'
    height='16'
    xmlns='http://www.w3.org/2000/svg'
    xmlns:svg='http://www.w3.org/2000/svg'>
-  <defs
-     id='defs8' />
-  <g
-     id='layer1'>
-    <path
-       style='fill:#000000'
-       id='path442'
-       d='m 10.397244,6.0803676 -5.6349021,3.2533125 -5.63490216,3.2533119 0,-6.5066246 0,-6.50662462 5.63490256,3.25331252 z'
-       transform='matrix(0.66549512,0,0,0.69160281,4.8306846,3.7948007)' />
-  </g>
+    <defs id='defs8' />
+    <polygon points='1,1 15,8 1,15'/>
 </svg>
 ".as_bytes()));
                         toggle_play = button(play)
@@ -278,17 +275,28 @@ impl Application for Player {
                 }
                 let row = row![
                     toggle_play,]
-                        .spacing(5)
-                        .padding(10)
-                        .height(input.height());
-                let col = column![];
-                let scroll = scrollable(
-                    container(col)
-                        .padding(20))
-                            .height(Length::Fill);
+                        .spacing(1)
+                        .padding(1);
+                let scroll = match tracks {
+                    Some(tracks) =>
+                    scrollable(
+                        column(
+                            tracks
+                                .iter()
+                                .map(|track|{
+                                    track.view()
+                                })
+                                .collect(),
+                        )
+                            .spacing(1)
+                            .padding(1)),
+                    None =>
+                    scrollable(
+                    container(text(""))) //change this later
+                };
                 // WIDGETS NOT ASSIGNED TO A MASTER CONTAINER WILL ERROR!!!!!
                 let container_col = column![input, scroll, row]
-                    .spacing(5);
+                    .spacing(1);
                 container(container_col).into()
             }
         }
@@ -298,49 +306,59 @@ impl Application for Player {
         subscription::events_with(|event, status| match (event, status) {
             (
                 Event::Keyboard(keyboard::Event::KeyPressed {
-                   key_code: keyboard::KeyCode::Down,
-                   modifiers,
+                    key_code: keyboard::KeyCode::Down,
+                    ..
                 }),
                 event::Status::Ignored,
             ) => Some(Message::DownPressed),
             (
                 Event::Keyboard(keyboard::Event::KeyPressed {
                     key_code: keyboard::KeyCode::Up,
-                    modifiers,
+                    ..
                 }),
                 event::Status::Ignored,
             ) => Some(Message::UpPressed),
             (
                 Event::Keyboard(keyboard::Event::KeyPressed {
                     key_code: keyboard::KeyCode::Enter,
-                    modifiers,
+                    ..
                 }),
                 event::Status::Captured,
             ) => Some(Message::EnterPressed),
-            (
-                Event::Window(window::Event::Resized {
-                    width,
-                    height,
-                }),
-                event::Status::Captured,
-            ) => Some(Message::Resized(width, height)),
             _ => None,
         })
     }
 }
 
-fn get_file_names (paths: Vec<PathBuf>) -> Vec<String> {
-    let mut return_string = Vec::new();
-    for path in paths {
-        let full_path = path.to_string_lossy();
-        let split_path: Vec<String> = Vec::from_iter(full_path.split(['/', '\\'].as_ref()).map(String::from));
-        return_string.push(String::from(&split_path[full_path.len() - 1])); // Shitty code
-    }
-    return return_string;
+#[derive(Debug, Clone)]
+struct Track {
+    path: PathBuf,
+    name: String
 }
 
-fn index_cur_directory(cur_directory: &String) -> Vec<PathBuf> {
-    let audio_index: Result<Vec<_>, _> = glob(&format!("{}{}", cur_directory, "/*.png")).expect("Failed to read glob pattern").collect();
+// A singular music track
+impl Track {
+    fn new(path: PathBuf) -> Self{
+        Track {
+            name: get_file_name(&path),
+            path: path,
+        }
+    }
+    fn view(&self) -> Element<Message>{
+        button(text(&self.name))
+            .on_press(Message::StartSong(self.path.clone()))
+            .into()
+    }
+
+}
+
+fn get_file_name(path: &PathBuf) -> String {
+    let mut full_path: Vec<String> = path.to_string_lossy().split(['/', '\\'].as_ref()).map(String::from).collect();
+    return full_path.pop().unwrap();
+}
+
+fn index_cur_directory(cur_directory: &PathBuf) -> Vec<PathBuf> {
+    let audio_index: Result<Vec<_>, _> = glob(&format!("{}{}", cur_directory.as_path().display().to_string(), "/*.mp3")).expect("Failed to read glob pattern").collect();
     let unwrapped_index = audio_index.unwrap();
     return unwrapped_index;
 }
